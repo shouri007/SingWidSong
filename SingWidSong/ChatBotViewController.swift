8e48f13aa82789fe0cb02e7a8748aa6538b0c23b
Shouri Piratla <shouri.piratla9@gmail.com>
2016-12-23 15:37:08 +0530

    Retrieval of JSON data

--- SingWidSong/ChatBotViewController.swift
+++ SingWidSong/ChatBotViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ChatBotViewController : UIViewController{
     
@@ -14,15 +15,50 @@ class ChatBotViewController : UIViewController{
     @IBOutlet var textField : UITextField?
     let searchString : String? = nil
     var base_url = "http://api.musixmatch.com/ws/1.1/"
-    var api_key = "bac80b7f06af437edc250ebe533c6b5c"
-    
+    var api_key = "&apikey=bac80b7f06af437edc250ebe533c6b5c"
+    var searchText : String?
     
     override func viewDidLoad() {
     }
     
+    func getSearchText() -> String{
+        searchText = textField?.text
+        return searchText!
+    }
+    
     @IBAction func search(){
         
-        
-    }
+        let text = getSearchText()
+        print(text)
+        let url = base_url + "track.get?track_id=15445219" + api_key
 
+        let request = URLRequest(url: URL(string: url)!)
+        let urlSession = URLSession.shared
+        let downloadTask = urlSession.downloadTask(with: request, completionHandler: {
+            (data,response,error)  -> Void in
+            if let error = error{
+                print(error)
+                return
+            }
+            do{
+                let res = try Data(contentsOf: data!)
+                self.parseJSON(data: res)
+            }catch{
+                print(error)
+            }
+        })
+        downloadTask.resume()
+    }
+    
+    func parseJSON(data : Data){
+        do{
+            let jsonResult = try JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
+            let jsonMessage = jsonResult["message"] as! [String:Any]
+            let jsonBody = jsonMessage["body"]
+            print(jsonBody)
+            
+        }catch{
+            print(error)
+        }
+    }
 }
