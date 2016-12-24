9a5cffe8d42aa9ec899a830074d27ee507abf0dc
Shouri Piratla <shouri.piratla9@gmail.com>
2016-12-24 11:52:33 +0530

    Create class for suggestions by LastFM

---
+++ SingWidSong/LastFmSuggestion.swift
@@ -0,0 +1,16 @@
+//
+//  LastFmSuggestion.swift
+//  SingWidSong
+//
+//  Created by Shouri on 24/12/16.
+//  Copyright © 2016 Shouri. All rights reserved.
+//
+
+import UIKit
+
+class LastFmSuggestion{
+    
+    var artist : String?
+    var name : String?
+    
+}
