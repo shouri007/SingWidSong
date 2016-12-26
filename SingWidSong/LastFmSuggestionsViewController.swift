8a7bdd2073162a5ee49884b86b794966260455cd
Shouri Piratla <shouri.piratla9@gmail.com>
2016-12-26 22:27:23 +0530

    Table View for LastFM Suggestions

--- SingWidSong/LastFmSuggestionsViewController.swift
+++ SingWidSong/LastFmSuggestionsViewController.swift
@@ -6,4 +6,38 @@
 //  Copyright © 2016 Shouri. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol LastFmSuggestionViewControllerDelegate {
+    
+    var num_rows : Int {get set}
+    func setNumberOfRows()
+}
+
+class LastFmSuggestionsViewController : UITableViewController{
+    
+    var num_rows = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.rowHeight = 80
+    }
+    
+    init(count : Int){
+        super.init(style: .grouped)
+        self.num_rows = count
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return num_rows
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 60
+    }
+}
